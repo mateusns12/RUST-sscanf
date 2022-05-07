2b0231d5403dacffd4d0f50951baf6fa18234f41
mateusns12 <mateusgms12@gmail.com>
2022-05-07 17:33:56 -0300

    changed scan_to_new to sscanf

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ mod scan{
         }
     }
 
-    pub macro scan_to_new ($buffer:expr,$sep:expr,[$($y:ty),+],$($x:expr),+){
+    pub macro sscanf ($buffer:expr,$sep:expr,[$($y:ty),+],$($x:expr),+){
         let res = splitter!($buffer,$sep);
         let mut i = 0;
         $(
@@ -26,18 +26,18 @@ fn main() {
     let a :u8;   let b :i32;   let c :i16;   let d :f32;
     let buffer = "02:98;76,39.6";
     let sep = [':',';',','];
-    scan::scan_to_new!(buffer,sep,[u8,i32,i16,f32],a,b,c,d);
+    scan::sscanf!(buffer,sep,[u8,i32,i16,f32],a,b,c,d);
     println!("{} {} {} {}",a,b,c,d);
 
     let a :u8;   let b :i32;   let c :i16;   let d :f32;   let e :String;
     let buffer = "02:98;abc,39.6";
     let sep = [':',';',',',' '];
-    scan::scan_to_new!(buffer,sep,[u8,i32,String,f32],a,b,e,d);
+    scan::sscanf!(buffer,sep,[u8,i32,String,f32],a,b,e,d);
     println!("{} {} {} {}",a,b,e,d);
 
     let buffer = "00:00:03,400 --> 00:00:06,177";
     let a :u8;   let b :u8;   let c :u8;   let d :i32;   let e :String;
     let f:u8; let g:u8; let h:u8; let i:i32;
-    scan::scan_to_new!(buffer,sep,[u8,u8,u8,i32,String,u8,u8,u8,i32],a,b,c,d,e,f,g,h,i);
+    scan::sscanf!(buffer,sep,[u8,u8,u8,i32,String,u8,u8,u8,i32],a,b,c,d,e,f,g,h,i);
     println!("{:02}:{:02}:{:02},{:03} --> {:02}:{:02}:{:02},{:03}",a,b,c,d,f,g,h,i);
 }
